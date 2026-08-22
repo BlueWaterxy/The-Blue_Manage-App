de517336144dfa6c3f21788452f28091e3cc921b
Santiago Avila <bluewater@MacBookPro.hsd1.ca.comcast.net>
2026-08-21 20:13:43 -0700

    Hello

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
-def main(): 
-    print("Welcome to The Blue Manage!")
+# Start program
+from grocery_manager import GroceryManager
+manager = GroceryManager()
 
+while True:
 
+    print("\n1. See groceries")
+    print("2. Buy grocery")
+    print("3. See what I bought")
+    print("4. Quit")
 
-main()
+    choice = input("Choose: ")
+    #   Shows the groceries in the list.
+    if choice == "1":
+        manager.show_groceries()
+    #  Adds a grocery to the list of groceries. 
+    if choice == "2":
+        manager.add_grocery()
+    #   Shows the total cost of the groceries bought.
+    if choice == "3":
+        manager.total_cost()
+
+    # Ends the program 
+    if choice == "4":
+        print("\n==============================")
+        print("Thanks for using BlueWater's Grocery Manager!")
+        print("==============================")
+        break
      
